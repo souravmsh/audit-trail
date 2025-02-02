950b0b4a0842170aa5f4c0d1707a8f21e707fb64
Shohrab Hossain <sourav.diubd@gmail.com>
2025-02-02 20:22:38 +0600

    pagination with query parameters

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -1,11 +1,11 @@
-@if (!empty($result->count()))
+@if ($result->count())
 <div class="text-right margin-20-auto">
     <div class="pagination">
         {{-- Previous Page Link --}}
         @if ($result->onFirstPage())
             <a href="#" disabled>&laquo;</a>
         @else
-            <a href="{{ $result->previousPageUrl() }}">&laquo; Prev</a>
+            <a href="{{ $result->previousPageUrl() . '&' . http_build_query(request()->all()) }}">&laquo; Prev</a>
         @endif
 
         {{-- Pagination Elements --}}
@@ -14,14 +14,14 @@
             @if ($page == $result->currentPage())
                 <a href="#" class="active">{{ $page }}</a>
             @else
-                <a href="{{ $url }}">{{ $page }}</a>
+                <a href="{{ $url . '&' . http_build_query(request()->all()) }}">{{ $page }}</a>
             @endif
         @endforeach
         @endif
 
         {{-- Next Page Link --}}
         @if ($result->hasMorePages())
-            <a href="{{ $result->nextPageUrl() }}">Next &raquo;</a>
+            <a href="{{ $result->nextPageUrl() . '&' . http_build_query(request()->all()) }}">Next &raquo;</a>
         @else
             <a href="#" disabled>&raquo;</a>
         @endif
