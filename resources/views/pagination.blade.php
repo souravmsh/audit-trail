da149415f1cea6894a4fd41bfbc4bf56c8387148
Shohrab Hossain <sourav.diubd@gmail.com>
2025-02-05 00:01:09 +0600

    Update pagination.blade.php

--- resources/views/pagination.blade.php
+++ resources/views/pagination.blade.php
@@ -1,30 +1,48 @@
 @if ($result->count())
-<div class="text-right margin-20-auto">
-    <div class="pagination">
-        {{-- Previous Page Link --}}
-        @if ($result->onFirstPage())
-            <a href="#" disabled>&laquo;</a>
-        @else
-            <a href="{{ $result->previousPageUrl() . '&' . http_build_query(request()->except('page')) }}">&laquo; Prev</a>
-        @endif
-
-        {{-- Pagination Elements --}}
-        @if ($result instanceof \Illuminate\Pagination\LengthAwarePaginator)
-        @foreach ($result->getUrlRange(1, $result->lastPage()) as $page => $url)
-            @if ($page == $result->currentPage())
-                <a href="#" class="active">{{ $page }}</a>
+    <div class="text-right margin-20-auto">
+        <div class="pagination">
+            {{-- Previous Page Link --}}
+            @if ($result->onFirstPage())
+                <a href="#" disabled>&laquo;</a>
             @else
-                <a href="{{ $url . '&' . http_build_query(request()->except('page')) }}">{{ $page }}</a>
+                <a href="{{ $result->previousPageUrl() . '&' . http_build_query(request()->except('page')) }}">&laquo; Prev</a>
             @endif
-        @endforeach
-        @endif
 
-        {{-- Next Page Link --}}
-        @if ($result->hasMorePages())
-            <a href="{{ $result->nextPageUrl() . '&' . http_build_query(request()->except('page')) }}">Next &raquo;</a>
-        @else
-            <a href="#" disabled>&raquo;</a>
-        @endif
+            {{-- Pagination Elements --}}
+            @if ($result instanceof \Illuminate\Pagination\LengthAwarePaginator)
+                @php
+                    $currentPage = $result->currentPage();
+                    $lastPage = $result->lastPage();
+                    $start = max(1, $currentPage - 2);
+                    $end = min($lastPage, $currentPage + 2);
+                @endphp
+
+                {{-- First Page Link --}}
+                @if ($start > 1)
+                    <a href="{{ $result->url(1) . '&' . http_build_query(request()->except('page')) }}">1</a>
+                @endif
+
+                {{-- Page Links --}}
+                @for ($page = $start; $page <= $end; $page++)
+                    @if ($page == $currentPage)
+                        <a href="#" class="active">{{ $page }}</a>
+                    @else
+                        <a href="{{ $result->url($page) . '&' . http_build_query(request()->except('page')) }}">{{ $page }}</a>
+                    @endif
+                @endfor
+
+                {{-- Last Page Link --}}
+                @if ($end < $lastPage)
+                    <a href="{{ $result->url($lastPage) . '&' . http_build_query(request()->except('page')) }}">{{ $lastPage }}</a>
+                @endif
+            @endif
+
+            {{-- Next Page Link --}}
+            @if ($result->hasMorePages())
+                <a href="{{ $result->nextPageUrl() . '&' . http_build_query(request()->except('page')) }}">Next &raquo;</a>
+            @else
+                <a href="#" disabled>&raquo;</a>
+            @endif
+        </div>
     </div>
-</div>
 @endif
